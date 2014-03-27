8940efc176ea7be4438658bba5da26da7a2f6668
k4m4r82 <k4m4r82@gmail.com>
2014-03-27 13:22:06 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntityFramework4.Models;
+
+namespace EntityFramework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new RetailContext())
+            {
+                var noUrut = 1;
+
+                var daftarSupplier = db.Suppliers.ToList();
+                foreach (var supplier in daftarSupplier)
+                {
+                    Console.WriteLine("{0}. {1}, {2}", noUrut, supplier.NamaSupplier, supplier.Jalan);
+
+                    noUrut++;
+                }
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
